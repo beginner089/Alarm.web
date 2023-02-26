8c041d1e51fd29c9fc4bdefbe342157c0fa430cf
beginner089 <125736909+beginner089@users.noreply.github.com>
2023-02-26 15:11:19 +0900

    01 commit
    練習で作りました。

---
+++ Alarm.web/alarm.js
@@ -0,0 +1,17 @@
+// var date = new Date();
+// var year = date.getFullYear();
+// var month = date.getMonth() + 1;
+// var day = date.getDate();
+// console.log(year+"年"+month+"月"+day+"日");
+// var date = new Date();
+// var hours = date.getHours();
+// var minutes = date.getMinutes();
+// var seconde = date.getSeconds() + 1;
+// console.log(hours+"時"+minutes+"分"+seconde+"秒");
+
+
+
+
+
+
+
